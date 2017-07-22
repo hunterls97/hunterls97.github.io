0aa198107f247c0e1de3b617065fe78d09d687fc
hunter liam <hunter.schofield@york.ca>
2017-07-22 16:43:39 -0400

    Basic Design Layout

--- js/slides.js
+++ js/slides.js
@@ -1,5 +1,7 @@
 var index = 0;
 
+var $j = $('.container');
+
 function next(){
 	index++;
 	index = index > 2 ? 0 : index;
@@ -14,5 +16,7 @@ function prev(){
 
 function dispSlide(index){
 	alert(index);
+	alert($j.text());
+	$('div.slide-container>img').remove();
 	$('div.slide-container').append('<img src="/static/img'+index+'.jpg" style="width:100%">').hide().fadeIn(1500);
 }
