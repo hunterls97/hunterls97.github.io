e8d60978655d6e28182e3d7f3f10a51bc9b63bb0
hunter liam <hunter.schofield@york.ca>
2017-07-22 16:34:06 -0400

    Basic Design Layout

--- js/slides.js
+++ js/slides.js
@@ -1,7 +1,17 @@
+var index = 0;
+
 function next(){
-	alert('next');
+	index++;
+	index = index > 3 ? 0 : index;
+	dispSlide(index);
 }
 
 function prev(){
-	alert('prev');
+	index--;
+	index = index < 0 ? 3 : index;
+	dispSlide(index);
+}
+
+function dispSlide(index){
+	alert(index);
 }
