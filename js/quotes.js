1aa79b09a70c0844f52f3bf0111f123e6a2a4a64
hunter liam <hunter.schofield@york.ca>
2017-07-22 22:36:54 -0400

    Basic Design Layout

--- js/quotes.js
+++ js/quotes.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 	$.get('/static/quotes.txt', function(data){
 		var quotes = data.split("%%");
-		for(i = 0; i < quotes.length; i++){
-			alert(quotes[i]);
-		}
+		var singleQuote = getRand(quotes);
+		alert(singleQuote);
 	});
 });
+
+function getRand(arr){
+	return arr[Math.floor(Math.random() * arr.length)];
+}
